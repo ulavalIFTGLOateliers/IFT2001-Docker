6e54656ba3dbef5e58efcc6caffee33df89e17ee
William Guimont-Martin <willGuimont@users.noreply.github.com>
2023-09-16 14:49:04 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
