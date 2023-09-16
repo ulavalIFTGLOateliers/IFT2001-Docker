aa8a4f424980d02a158a4ab103fd98a4aa8a0eb6
William Guimont-Martin <willGuimont@users.noreply.github.com>
2023-09-16 13:56:21 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,18 @@
+sudo apt update; sudo apt install -y python3-pip git curl vim debootstrap
+
 # Install docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt update
+sudo apt install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-
-git clone https://github.com/willGuimont/ateliers-iftglo-2001-server ~/server
